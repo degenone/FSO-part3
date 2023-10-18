d6ca2619e3e77b19db6f6cb05988af57cc7ab465
Tero Kilpeläinen <degenone1@gmail.com>
2023-10-18 17:55:47 +0300

    exercise 3.20 done

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const URL = process.env.MONGODB_URL;
+const NUMBER_RE = /\d{3}[\s-]\d{3}[\s-]\d{4}/;
 
 mongoose.set('strictQuery', false);
 
@@ -14,10 +15,24 @@ mongoose
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
-        minLength: 3,
+        minLength: [
+            3,
+            "'{VALUE}' is too short. Valid name must be at least 3 chacaters.",
+        ],
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: [
+            12,
+            "'{VALUE}' is too short. Valid number must be at least 12 chacaters.",
+        ],
+        validate: {
+            validator: (v) => NUMBER_RE.test(v),
+            message: (props) => `${props.value} is not a valid number`,
+        },
+        required: true,
+    },
 });
 personSchema.set('toJSON', {
     transform: (doc, returnObj) => {
